d955cb710d0bdfa02603cc2d35890739f4fb4467
Milad Dehghan <milad.dehghan@hotmail.com>
2022-09-28 05:57:37 +0330

    Fix non alphabets

--- main.py
+++ main.py
@@ -11,25 +11,27 @@ if setting == '2':
     encrypted = ''
 
     for word in words:
-        letter_index = letters.index(word.upper()) + 1
-        new_pos = letter_index + 13
-        if new_pos <= 26:
-            encrypted += letters[new_pos - 1]
-        else:
-            encrypted += letters[new_pos-27]
-
+        if word.upper() in letters:
+            letter_index = letters.index(word.upper()) + 1
+            new_pos = letter_index + 13
+            if new_pos <= 26:
+                encrypted += letters[new_pos - 1]
+            else:
+                encrypted += letters[new_pos-27]
+        else:                                                                                                  │
+            decrypted += word
     print(encrypted)
 else:
     decrypted = ''
 
     for word in words:
-        letter_index = letters.index(word.upper()) + 1
-        new_pos = letter_index - 13
-        if new_pos >= 0:
-            decrypted += letters[new_pos - 1]
+        if word.upper() in letters:
+            letter_index = letters.index(word.upper()) + 1
+            new_pos = letter_index - 13
+            if new_pos >= 0:
+                decrypted += letters[new_pos - 1]
+            else:
+                decrypted += letters[25 - abs(new_pos)]
         else:
-            print(letter_index)
-            print(new_pos)
-            print(26 - abs(new_pos))
-            decrypted += letters[25 - abs(new_pos)]
+            decrypted += word
     print(decrypted)
